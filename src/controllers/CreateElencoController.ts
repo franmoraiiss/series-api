be7c87873a181da35d2f21ebdb50ab7f9d62ca23
Gustavo Salles <87676513+salles97@users.noreply.github.com>
2023-06-16 14:25:16 -0300

    Api-Series

--- src/controllers/CreateElencoController.ts
+++ src/controllers/CreateElencoController.ts
@@ -7,8 +7,8 @@ export class CreateElencoController {
 
     const elenco = await prismaClient.elenco.create({
       data: {
-        pessoa_idPessoa: idPessoa,
-        serie_idSerie: idSeries,
+        pessoa_id_pessoa: idPessoa,
+        serie_id_serie: idSeries,
         personagem: personagem,
       },
     });
